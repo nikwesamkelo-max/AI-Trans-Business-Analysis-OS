255e1d709a1fa88f39b3107509c04884b382107f
nikwesamkelo-max <nikwesamkelo@gmail.com>
2026-07-02 15:29:35 +0200

    Import database functions for analytics

--- analytics.py
+++ analytics.py
@@ -1,1 +1,9 @@
-
+from database import (
+    get_total_trips,
+    get_completed_trips,
+    get_active_trips,
+    get_cancelled_trips,
+    get_total_revenue,
+    get_total_fuel_cost,
+    get_top_customers
+)
